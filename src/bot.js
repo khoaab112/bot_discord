e03701fab0b804243a9b0e2f8e4a5312c16a0f06
TIA <97724277+khoaab112@users.noreply.github.com>
2024-03-07 14:06:11 +0700

    fix  message content is empty

--- src/bot.js
+++ src/bot.js
@@ -1,31 +1,28 @@
 const TOKEN = process.env.TOKEN;
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Partials } = require('discord.js');
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.DirectMessages,
-    ]
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildBans,
+    ],
+    partials: [Partials.Channel],
 });
 
 function responseCode() {
-    // client.on('ready', () => {
-    //     console.log(`Logged in as ${client.user.tag}!`);
-    // });
-
-    // client.on('interactionCreate', async interaction => {
-    //     console.log(`Logged in as ${client.user.tag}`);
-    //     if (!interaction.isChatInputCommand()) return;
-
-    //     if (interaction.commandName === 'ping') {
-    //         await interaction.reply('Pong!');
-    //     }
-    // });
-
-    client.on('messageCreate', async function(message) {
-        console.log(message);
-        // await message.reply('Pong!');
+    client.on('messageCreate', (msg) => {
+        if (msg.author.bot) return;
+        console.log(msg);
+        if (msg.content === 'Hello') {
+            msg.reply(`Hello ${msg.author.username}`);
+        }
     });
+    // client.on('messageCreate', async function(message) {
+    //     console.log(message);
+    //     // await message.reply('Pong!');
+    // });
     client.on('directMessageCreate', (message) => {
         // Xử lý tin nhắn riêng
         console.log(message);
